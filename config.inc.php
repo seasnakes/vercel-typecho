7ae262f9740122a4d3f318424a453c774dd1cf4f
DT-DN-083 <duitang@78.170.169.192.host.secureserver.net>
2024-04-10 18:58:26 +0800

    Vercel-Deploy-Typecho

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户名',
-  'password' => '数据库密码',
+  'host' => 'rm-bp1u1fru3rkg8m5y53o.mysql.rds.aliyuncs.com',
+  'user' => 'snake',
+  'password' => 'Zhang586421',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库昵称',
+  'database' => 'testdb',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
